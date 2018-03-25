3d6298df6b0fad1e715d399684482ff8d4888ca8
matejbuzas <matej.buzas@gmail.com>
2018-03-25 22:28:22 +0200

    citanie vstupu

--- src/extremko/Main.java
+++ src/extremko/Main.java
@@ -1,6 +1,9 @@
 package extremko;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  *
@@ -8,15 +11,78 @@ import java.io.IOException;
  */
 public class Main {
     private static Playground playground;
-
-    public static void main(String[] args) throws IOException {
+    private static Scanner reader;
+    
+    
+    //java -jar nazov.jar
+    public static void main(String[] args) throws IOException, InterruptedException {
+        reader = new Scanner(System.in); 
         createPlayground();
+        play();
+        
+        
+        
+    }
+    
+    public static void play() throws IOException, InterruptedException {  
+        menu();
+        
+        
+       
+    }
+    
+    public static void mesto() throws IOException, InterruptedException {  
+        clear();
+        System.out.println("Tvoje mesto");
+        System.out.println("--------------------------------------");
+        System.out.println("1 - navrat do menu"); 
+        int n = reader.nextInt();
+        if (n == 1) {
+            play();
+        }
+       
+    }
+    
+    public static void susedia() throws IOException, InterruptedException {  
+        clear();
+        Scanner reader = new Scanner(System.in); 
+        System.out.println("Susedia");
+        System.out.println("--------------------------------------");
+        System.out.println("1 - navrat do menu"); 
+        int n = reader.nextInt();
+        if (n == 1) {
+            play();
+        }
+    }
+    
+    public static void menu() throws IOException, InterruptedException {
+        clear();
+        System.out.println("Vitaj v hre!");
+        System.out.println("--------------------------------------");
+        System.out.println("1 - Zobraz svoje mesto");
+        System.out.println("2 - Zobraz susedov"); 
+        int n = reader.nextInt();
+        
+        if (n == 1) {
+            mesto ();
+        }
+        else if (n == 2) {
+            susedia();
+        }
     }
     
     public static void createPlayground() throws IOException {
         playground = new Playground();
-        playground.printMap();
+        //playground.printMap();
     }
+    
+    public static void clear() throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        
+    }
+
     
     
 }
+
+
