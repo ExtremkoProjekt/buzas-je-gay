3fc658017b1e27b024c5395e483ac0a8c6a5d243
matejbuzas <matej.buzas@gmail.com>
2018-03-27 15:31:54 +0200

    user repository

--- src/database/UserRepository.java
+++ src/database/UserRepository.java
@@ -1,9 +1,38 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author MATEJ
  */
 public class UserRepository {
+    //adds user to database
+     public static void addUser(String name) throws ClassNotFoundException, SQLException {
+        Connection c = DatabaseConnection.getConnection();
+        c.setAutoCommit(false);
+        String sql = "INSERT INTO USER (USER_ID) VALUES (?);"; 
+        PreparedStatement pstmt;
+        pstmt = c.prepareStatement(sql);
+        pstmt.setString(1, name);
+        pstmt.executeUpdate();
+        pstmt.close();
+        c.commit();
+        c.setAutoCommit(true);
+    }
+    
+     //returns true if user with name in parameter exists
+    public static boolean userExists(String name) throws ClassNotFoundException, SQLException {
+        Connection c = DatabaseConnection.getConnection();
+        PreparedStatement pstmt;
+        String sql = "SELECT * FROM USER WHERE NAME = ?;";
+        pstmt = c.prepareStatement(sql);
+        pstmt.setString(1, name);
+        ResultSet rs = pstmt.executeQuery();
+        return rs.next();  
+    }
     
 }
