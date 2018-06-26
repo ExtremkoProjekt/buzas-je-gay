e0254bcdfa5b30d0325a451e6e5df386150d8b0d
Martin Sadlon <m.sadlon93@gmail.com>
2018-06-27 00:51:10 +0200

    testy ver.0.0.1

--- test/DummyTest.java
+++ test/DummyTest.java
@@ -3,6 +3,7 @@
 import database.BootstrapDB;
 import database.DatabaseHandleTables;
 import entities.Building;
+import entities.BuildingProgress;
 import entities.User;
 import extremko.Playground;
 import java.io.FileNotFoundException;
@@ -55,17 +56,12 @@ public class DummyTest {
         assertEquals(0, pg.towns.size());
     }
 
-
     @Test
     public void testLoadTown2() throws IOException, ClassNotFoundException, SQLException{
         Playground pg = new Playground();
         pg.parseTowns("a");
         assertEquals(1, pg.towns.size());
     }
-
-
-
-
 
     @Test
     public void testAddUser() throws IOException, ClassNotFoundException, SQLException{
@@ -78,36 +74,49 @@ public class DummyTest {
         assertEquals(2,UserRepository.userCount());
     }
 
-
-
-
-
-
     @Test
     public void testAddTown() throws IOException, ClassNotFoundException, SQLException{
-
         User u = UserRepository.getUserByName("stefan");
         UserRepository.getEnemies("stefan");
         //System.out.println(u.getName()+" "+u.getUserID());
-
         TownRepository.add("mestoStefan",1);
-
-
         assertEquals("mestoStefan",TownRepository.getTownNameByUserID(1));
     }
 
     @Test
     public void testBuildingsCount() throws IOException, ClassNotFoundException, SQLException{
-
-
         assertEquals(0,BuildingRepository.buildingCount());
-
         BuildingRepository.add("Kasarne");
-
         assertEquals(1,BuildingRepository.buildingCount());
-
         //DatabaseHandleTables.dropTables();
     }
+
+    //----------------------------------------
+    
+    @Test
+    public void testBuildingProgress() throws IOException, ClassNotFoundException, SQLException{
+        BuildingProgress bp = new BuildingProgress(1, 1, 10, 5, 1);
+        assertEquals("KASAREN",bp.getTypeOfBuidling(bp.getId()));
+    }
+
+    @Test
+    public void testBuildingProgress2() throws IOException, ClassNotFoundException, SQLException{
+        BuildingProgress bp = new BuildingProgress(2, 1, 20, 10, 2);
+        assertEquals("ZLATA_BANA",bp.getTypeOfBuidling(bp.getId()));
+    }
+
+    @Test
+    public void testBuildingProgress3() throws IOException, ClassNotFoundException, SQLException{
+        BuildingProgress bp = new BuildingProgress(3, 1, 25, 15, 4);
+        assertEquals("HLAVNA_BUDOVA",bp.getTypeOfBuidling(bp.getId()));
+    }
+
+    @Test
+    public void testBuildingProgress4() throws IOException, ClassNotFoundException, SQLException{
+        BuildingProgress bp = new BuildingProgress(0, 1, 25, 15, 4);
+        assertEquals("NULL",bp.getTypeOfBuidling(bp.getId()));
+    }
+
 
 
 
